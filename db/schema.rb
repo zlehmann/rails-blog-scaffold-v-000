f51bd9ac046952dfed5925b0c8fce8a58a8bd419
ahimmelstoss <ahimmelstoss@gmail.com>
2014-09-22 16:15:05 -0400

    solution

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,6 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-
+ActiveRecord::Schema.define(version: 20_140_922_193_819) do
+  create_table 'posts', force: true do |t|
+    t.string   'title'
+    t.datetime 'created_at'
+    t.datetime 'updated_at'
+  end
 end
