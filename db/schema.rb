59ddbaa55ec3ab4d317cd8b3242380eb819d1e6a
Zachary Lehmann <zlehmann@gmail.com>
2019-01-19 18:39:42 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190119183853) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
 end
